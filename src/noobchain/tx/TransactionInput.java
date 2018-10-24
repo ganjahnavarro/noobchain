dc38af7297a333fb0dd3d7bc2989d87fe85ff3cc
ganjahnavarro <ganjahnavarro@gmail.com>
2018-10-25 05:28:58 +0800

    update tx input and output class

--- src/noobchain/tx/TransactionInput.java
+++ src/noobchain/tx/TransactionInput.java
@@ -1,5 +1,28 @@
 package noobchain.tx;
 
+/**
+ * 
+ * This class will be used to reference TransactionOutputs that have not yet
+ * been spent. The transactionOutputId will be used to find the relevant
+ * TransactionOutput, allowing miners to check your ownership.
+ *
+ */
 public class TransactionInput {
+
+	// Contains the Unspent transaction output
+	private TransactionOutput UTXO;
+	private String transactionOutputId;
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+
+	public TransactionOutput getUTXO() {
+		return UTXO;
+	}
+
+	public String getTransactionOutputId() {
+		return transactionOutputId;
+	}
 
 }
--- src/noobchain/tx/TransactionOutput.java
+++ src/noobchain/tx/TransactionOutput.java
@@ -1,5 +1,56 @@
 package noobchain.tx;
 
+import java.security.PublicKey;
+
+import noobchain.util.StringUtil;
+
+/**
+ * 
+ * This class will show the final amount sent to each party from the
+ * transaction. These, when referenced as inputs in new transactions, act as
+ * proof that you have coins to send.
+ *
+ */
 public class TransactionOutput {
+
+	private String id;
+
+	// New owner of the coins
+	private PublicKey recipient;
+
+	// Amount of coins they own
+	private float value;
+
+	// ID of the transaction this output was created in
+	private String parentTransactionId;
+
+	public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+		this.recipient = recipient;
+		this.value = value;
+		this.parentTransactionId = parentTransactionId;
+		this.id = StringUtil
+				.applySha256(StringUtil.getStringFromKey(recipient) + Float.toString(value) + parentTransactionId);
+	}
+
+	// Check if coin belongs to you
+	public boolean isRecipientSelf(PublicKey publicKey) {
+		return (publicKey == recipient);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public PublicKey getRecipient() {
+		return recipient;
+	}
+
+	public float getValue() {
+		return value;
+	}
+
+	public String getParentTransactionId() {
+		return parentTransactionId;
+	}
 
 }
